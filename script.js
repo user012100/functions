51502f6061e29f0a50197e254128b228e2817ebe
Ali Salifov <asalifov@gmail.com>
2026-04-01 12:33:04 -0400

    preventing page from reloading on submit button press

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-let writingArea = document.getElementById('poem');
+let writingArea = document.getElementById('poem-form');
 
+// following the example from https://github.com/typography-interaction-2526/forms-params-storage
+let storePoems = () => {
+	// https://developer.mozilla.org/en-US/docs/Web/API/FormData
+	let savePoem = new FormData(writingArea)
+
+	savePoem.forEach((value, key) => {
+		// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+		localStorage.setItem(key, value)
+	})
+}
+
+writingArea.addEventListener('submit', (event) => {
+	// https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+	event.preventDefault()
+})
