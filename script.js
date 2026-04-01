82c1bb08b7b9fbfe5724cdea9959eb6903d3fd7c
Ali Salifov <asalifov@gmail.com>
2026-04-01 13:08:20 -0400

    added titles for poem submission and function to retrieve them from localStorage

--- script.js
+++ script.js
@@ -2,18 +2,35 @@ let writingArea = document.getElementById('poem-form');
 
 // following the example from https://github.com/typography-interaction-2526/forms-params-storage
 
+let retrievePoems = () => {
+	let poems = []
+
+	// using Object.keys() here instead of cycling through localStorage with a simple for loop (just thought this was a better way of doing it): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+	Object.keys(localStorage).forEach((key) => {
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+		poems.push({
+			id: key,
+			text: localStorage.getItem(key)
+		})
+	})
+
+	console.log(poems)
+}
+
 let writePoem = () => {
 	let poemText = document.getElementById('poem-text').value
 
-	let poemID = localStorage.length + 1
+	let poemTitle = document.getElementById('poem-title').value
 
-	// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
-	localStorage.setItem(poemID, poemText)
+	// https://developer.mozilla.orgq/en-US/docs/Web/API/Window/localStorage
+	localStorage.setItem(poemTitle, poemText)
 }
 
 writingArea.addEventListener('submit', (event) => {
 	// https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
 	event.preventDefault()
-
+	
 	writePoem()
 })
+
+retrievePoems()
