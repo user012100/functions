9dc71e1bef511ba8c6fa10092b304289cefab957
Ali Salifov <asalifov@gmail.com>
2026-04-01 12:44:52 -0400

    removed example

--- script.js
+++ script.js
@@ -2,21 +2,12 @@ let writingArea = document.getElementById('poem-form');
 
 // following the example from https://github.com/typography-interaction-2526/forms-params-storage
 
-let storePoems = () => {
-	// https://developer.mozilla.org/en-US/docs/Web/API/FormData
-	let savePoem = new FormData(writingArea)
-
-	savePoem.forEach((value, key) => {
-		// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
-		localStorage.setItem(key, value)
-	})
-}
-
 let writePoem = () => {
 	let poemText = document.getElementById('poem-text').value
 
 	let poemID = localStorage.length + 1
 
+	// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
 	localStorage.setItem(poemID, poemText)
 }
 
